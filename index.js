c2cc16bbdc74bd8e0983a284771cda51f30c3f3b
jtreeves <jackson.t.reeves@gmail.com>
2020-11-12 15:02:14 -0500

    [js] Module practice

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const { beBasic, add, subtract } = require('./myModule')
+
+// THIS SHOULD WORK AS A REPLACEMENT FOR THE ABOVE, BUT IT DOESN'T
+// const myModule = require('./myModule.js')
+
 let name = 'Jackson Reeves'
 console.log(name)
 
@@ -6,3 +11,9 @@ function printName(person) {
 }
 
 console.log(printName(name))
+
+console.log(beBasic())
+
+console.log(add(5, 50))
+
+console.log(subtract(5, 50))
